1792d724c4c581f410af06b4e5428d086ad302d9
Sergio Garcia <alexalihord@gmial.com>
2026-03-11 15:36:54 +0000

    agregamos codigo e imagen

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<?php
+echo "Hola mundo, soy Sergio García";
+?>
+
+<br><br>
+
+<img src="/Proyecto_php/arthasia.jpg" alt="Arthasia logo or graphic element showcasing PHP web development technology in a professional design setting" width="300">
+
+
+
+
+
